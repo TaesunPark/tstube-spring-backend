a5a5b1a462cb7dafff21e40b184fdffcd323bc9c
taesunpark <tovbskvb@daum.net>
2025-01-04 04:08:17 +0900

    [feat] 운영계 설정 변경

--- src/main/java/com/example/global/config/WebConfig.java
+++ src/main/java/com/example/global/config/WebConfig.java
@@ -10,7 +10,9 @@ public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
-			.allowedOrigins("http://localhost:3000")
+			.allowedOrigins("http://www.xn----bv7eq1qhzbe7i6wn.shop",
+							"http://localhost:3000"
+			)
 			.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
 			.allowedHeaders("*")
 			.allowCredentials(true);
