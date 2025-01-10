59248e2143612f200ef02fdf17c3b1847a2474d1
taesunpark <tovbskvb@daum.net>
2025-01-10 23:37:48 +0900

    [chore] 로컬 환경 설정 추가

--- src/main/java/com/example/global/config/WebConfig.java
+++ src/main/java/com/example/global/config/WebConfig.java
@@ -10,11 +10,13 @@ public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
-			.allowedOrigins("http://www.xn----bv7eq1qhzbe7i6wn.shop",
-							"http://localhost:3000"
+			.allowedOrigins(
+				"http://www.xn----bv7eq1qhzbe7i6wn.shop", // 허용할 도메인 1
+				"http://localhost:3000"                  // 허용할 도메인 2
 			)
-			.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-			.allowedHeaders("*")
-			.allowCredentials(true);
+			.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용 메서드
+			.allowedHeaders("*")                                      // 허용 헤더
+			.exposedHeaders("Authorization", "Content-Type")         // 클라이언트에 노출할 헤더
+			.allowCredentials(true);                                  // 쿠키 및 인증 정보 허용
 	}
 }
