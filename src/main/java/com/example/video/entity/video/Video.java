4553ec70c540f4b57f700b199c317c79c2423cef
taesunpark <tovbskvb@daum.net>
2025-04-05 23:50:20 +0900

    [bugfix] video 저장 시 user_id 저장 안되는 문제 수정 - @JoinColumn(name = "user_id", insertable = false, updatable = false) 에서 insert, update false여서 발생한 문제

--- src/main/java/com/example/video/entity/video/Video.java
+++ src/main/java/com/example/video/entity/video/Video.java
@@ -45,7 +45,7 @@ public class Video {
     private LocalDateTime updateTime;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id", insertable = false, updatable = false)
+    @JoinColumn(name = "user_id")
     private User user;
 
     @OneToOne(cascade = CascadeType.ALL)
